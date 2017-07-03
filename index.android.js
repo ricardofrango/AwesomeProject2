d41f701ae4e13e3c7247139c309dbe55e633b64e
Ricardo Frango <ricardo.frango@closer.pt>
2017-07-03 16:22:24 +0100

    first commit

---
+++ index.android.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class AwesomeProject2 extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject2', () => AwesomeProject2);
